852a29112009986f63b918ed85e53315ae5a9e9a
redrickh <104272075+redrickh@users.noreply.github.com>
2022-04-27 16:20:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-"""
-for future kivy gui
-
-from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDRectangleFlatButton, MDFlatButton
-from kivy.lang import Builder
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.label import MDLabel
-# import stilus
-"""
-
-
 import random
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 
-"""We need one excel file(kerdesek.xlsx), then we will add quesiton from B1 column(B2,B3 etc...).
-Our answer will be right below them. In this code we can write four answers down.
+"""We need one excel file(kerdesek.xlsx), then we will add questions from B1 to x1 columns (B1,C1 etc...).
+Our answers will be right below them.The correct answer will always be the second row(B2, C2 etc...). 
+We will shuffle them later, so it won't be in the same position when we print them to the user. 
+In this code we can write four answers down.
 So it will look like something like this:
 Question1  Question2  Question3
-answer1    same1      same
+Correct1    correct      same
 answer2    ....       ....
 answer3    ....       ....
-answer4    same4      ....
+answer4    answer4     ....
 """
 
 
-class Quizz:
+class Quiz:
 
     df = pd.read_excel("kerdesek.xlsx")  # reading our question and answers xlsx
 
