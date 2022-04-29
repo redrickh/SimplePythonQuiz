62c28ac8519f4562ae2954f26939ab87fdf28f07
redrickh <104272075+redrickh@users.noreply.github.com>
2022-04-29 19:12:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,75 @@
 import random
 import pandas as pd
+import ast
+from keyboard import press
 pd.options.mode.chained_assignment = None  # default='warn'
 
-"""We need one excel file(kerdesek.xlsx), then we will add questions from B1 to x1 columns (B1,C1 etc...).
+"""We need one excel file(kerdesek.xlsx), then we will add questions from A1 to x1 columns (B1,C1 etc...).
 Our answers will be right below them.The correct answer will always be the second row(B2, C2 etc...). 
 We will shuffle them later, so it won't be in the same position when we print them to the user. 
 In this code we can write four answers down.
 So it will look like something like this:
 Question1  Question2  Question3
 Correct1    correct      same
-answer2    ....       ....
+answer2    ....       ...
 answer3    ....       ....
 answer4    answer4     ....
 """
 
 
-class Quiz:
+class Main:
+    def quiz(self):
+        df = pd.read_excel("kerdesek.xlsx")  # reading our xlsx
+        question_numbers = len(df.columns)
+        question_number = int(question_numbers - 1)
+        self.randomvalue = random.randint(0, question_number)
 
-    df = pd.read_excel("kerdesek.xlsx")  # reading our question and answers xlsx
+        with open("numbers.txt", "r") as c:
+            x = c.readlines()  # read into a list
+            x = [ast.literal_eval(x[i].rstrip('\n')) for i in range(len(x))]
 
-    randomvalue = random.randint(1, 4)  # random number(from 1 to 4)
-    df2 = df.iloc[0:, randomvalue]  # choosing Columns and listing
-    random.shuffle(df2)  # randomize list
+        range_number = list(range(0, question_number + 1))
+        x.sort()
+        while self.randomvalue in x:
+            self.randomvalue = random.randint(0, question_number)
+            if x == range_number:
+                new_game = input("Nincs több kérdés. Újra játszol? y/n: ")
+                if new_game == "y" or "yes" or "igen" or "i" or press("enter"):
+                    self.new_game()
+                else:
+                    quit()
+                break
+                
+        df2 = df.iloc[0:, self.randomvalue]  # choosing columns and listing
+        random.shuffle(df2)  # randomize list
+        df3 = pd.read_excel("kerdesek.xlsx")
+        inputChoice = df3.iloc[:, self.randomvalue].head(
+            1)  # choosing from the original column and also choosing the fist row(answer)
+        question_ask = df2  # randomized question list
+        # print(str(inputChoice))
+        print("Válaszok:--------------")
+        print(question_ask)  # Printing df2 var. random column list
+        print("---------------------")
+        choosing = str(input("Válasz: "))  # user input
+        if choosing in str(
+                inputChoice):  # if the inputChoice value is in the user input value, then the answer is correct
+            print("Helyes")
+            self.already_asked()
+            return self.quiz
+        else:
+            print("Helytelen")  # it wasn't correct whops
 
-    df3 = pd.read_excel("kerdesek.xlsx")
-    inputChoice = df3.iloc[0:, randomvalue].head(1) # choosing from the original column and also choosing the fist row(answer)
-    question_ask = df2  # randomized questions list
+    def already_asked(self):
+        self.save_it = open("numbers.txt", "a")
+        self.save_it.write(str(self.randomvalue))
+        self.save_it.write("\n")
+        self.save_it.close()
 
-    print("Válaszok:--------------")
-    print(question_ask)  # Printing df2 var. random column list
-    print("---------------------")
-    choosing = str(input("Válasz: "))  # user input
+    def new_game(self):
+        txt = open("numbers.txt", "w")
+        txt.write("")
+        return self.quiz
 
-    if choosing in str(inputChoice):  # if the inputChoice value is in the user input value, then the answer is correct
-        print("Helyes")
 
-    else:
-        print("Helytelen")  # it wasn't correct whops
-
+my_object = Main()
+my_object.quiz()
